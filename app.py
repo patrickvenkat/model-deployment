5db9b4077b7a1c19d1722aa1c73bc9e16acecaae
patrickvenkat <51687587+patrickvenkat@users.noreply.github.com>
2025-09-06 16:13:59 +0000

    Updates to all files

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request # type: ignore
+import pickle
 
 app = Flask(__name__)
+cv = pickle.load(open("models/cv.pkl", "rb"))
+clf = pickle.load(open("models/clf.pkl", "rb"))
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def home():
-    text = ""
-    
-    if request.method == 'POST':
-        text = request.form.get('email-content')
-    
-    return render_template('spam_classifier.html', text=text)
+    return render_template("index.html")
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    email = request.form.get('email-content')
+    tokenized_email = cv.transform([email]) # X 
+    prediction = clf.predict(tokenized_email)
+    prediction = "Not Spam" if prediction == 1 else "Spam"
+    
+    return render_template("index.html", prediction=prediction, email=email)
 
 # Run the Flask application
 if __name__ == '__main__':
