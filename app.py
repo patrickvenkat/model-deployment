6c61cf13cc52b27dc2023556f129a60d2789b7ef
patrickvenkat <51687587+patrickvenkat@users.noreply.github.com>
2025-09-06 15:19:43 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    text = ""
+    if request.method == 'POST':
+        text = request.form.get('email-content')
+    return render_template('spam_classifier.html', text=text)
+
+
+# Run the Flask application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
