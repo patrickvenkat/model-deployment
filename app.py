984de3aa77dad167cb2d3fef0f1aad7e7a4785e2
patrickvenkat <51687587+patrickvenkat@users.noreply.github.com>
2025-09-07 18:20:16 +0000

    Code refactor with utils.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, jsonify, request # type: ignore
-import pickle
+from utils import make_prediction
 
 app = Flask(__name__)
-cv = pickle.load(open("models/cv.pkl", "rb"))
-clf = pickle.load(open("models/clf.pkl", "rb"))
 
 @app.route("/", methods=['GET'])
 def home():
@@ -12,21 +10,14 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     email = request.form.get('email-content')
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    prediction = 1 if prediction == 1 else -1
-    
+    prediction = make_prediction(email)
     return render_template("index.html", prediction=prediction, email=email)
-
 
 @app.route('/api/predict', methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)  # Get data posted as a json
     email = data['content']
-    tokenized_email = cv.transform([email])
-    prediction = clf.predict(tokenized_email)
-    # If the email is spam prediction should be 1
-    prediction = 1 if prediction == 1 else -1
+    prediction = make_prediction(email)
     return jsonify({'prediction': prediction, 'email': email})  # Return prediction
 
 # Run the Flask application
---
+++ utils.py
@@ -0,0 +1,10 @@
+import pickle
+
+cv = pickle.load(open("models/cv.pkl", "rb"))
+clf = pickle.load(open("models/clf.pkl", "rb"))
+
+def make_prediction(email):
+    tokenized_email = cv.transform([email]) # X 
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return prediction
